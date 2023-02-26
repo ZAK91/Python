69cdf5e41058ab6ae358cb7d980e874b5481e2ce
ZAK91 <120594345+ZAK91@users.noreply.github.com>
2023-02-26 08:57:57 +0100

    operators

---
+++ operators.py
@@ -0,0 +1,6 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('the = is Assignment Operator and the == is comparison operator')
+the = is Assignment Operator and the == is comparison operator
+>>> print('the != means, check if the value of two operands is not equal , if not then it is True')
+the != means, check if the value of two operands is not equal , if not then it is True
