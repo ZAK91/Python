96a5f102197dd8fc5f0d500ce5c3569321d60d02
ZAK91 <120594345+ZAK91@users.noreply.github.com>
2023-02-26 10:30:59 +0100

    all tasks done

--- operators.py
+++ operators.py
@@ -1,8 +1,8 @@
 Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> print('the = is Assignment Operator and the == is comparison operator')
+print('the = is Assignment Operator and the == is comparison operator')
 the = is Assignment Operator and the == is comparison operator
->>> print('the != means, check if the value of two operands is not equal , if not then it is True')
+print('the != means, check if the value of two operands is not equal , if not then it is True')
 the != means, check if the value of two operands is not equal , if not then it is True
 >>> print('Operator precedence determines how operators are parsed concerning each other. Operators with higher precedence become the operands of operators with lower precedence.')
 Operator precedence determines how operators are parsed concerning each other. Operators with higher precedence become the operands of operators with lower precedence.
@@ -21,3 +21,20 @@ Operator precedence determines how operators are parsed concerning each other. O
 >>> x=x*10
 >>> x
 50.0
+>>> x=x%3
+>>> x
+2.0
+>>> print(11%4)
+3
+>>> print(2**3)
+8
+>>> 11/3
+3.6666666666666665
+>>> x=int
+>>> x=11/3
+>>> x
+3.6666666666666665
+>>> 11//3
+3
+>>> print('yes we can divide 11 by 3 and get an integer number by using the floor division')
+yes we can divide 11 by 3 and get an integer number by using the floor division
