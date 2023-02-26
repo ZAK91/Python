f916515211277086393b64762a0157620618e345
ZAK91 <120594345+ZAK91@users.noreply.github.com>
2023-02-26 10:11:53 +0100

    5 task

--- operators.py
+++ operators.py
@@ -4,3 +4,17 @@ Type "help", "copyright", "credits" or "license()" for more information.
 the = is Assignment Operator and the == is comparison operator
 >>> print('the != means, check if the value of two operands is not equal , if not then it is True')
 the != means, check if the value of two operands is not equal , if not then it is True
+>>> print('Operator precedence determines how operators are parsed concerning each other. Operators with higher precedence become the operands of operators with lower precedence.')
+Operator precedence determines how operators are parsed concerning each other. Operators with higher precedence become the operands of operators with lower precedence.
+>>> x=5
+>>> x=10
+>>> x=10+5
+>>> x
+15
+>>> x=15+10
+>>> 
+>>> x
+25
+>>> x=x/5
+>>> x
+5.0
