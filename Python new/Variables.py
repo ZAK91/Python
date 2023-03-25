8bb21ff96c13203608730bb29d223ee2708a6680
ZAK91 <120594345+ZAK91@users.noreply.github.com>
2023-03-25 17:16:05 +0100

    print commands

--- Python new/Variables.py
+++ Python new/Variables.py
@@ -16,3 +16,7 @@ a = (10,11,12,13,14,15)
 type(a)
 a={1,2,3,4,5}
 a={1: 'zakria' , 2 : 'ahmad' , 3: 'mohammad' , 4 : 'yahya' , 5 : 'jebrel'}
+print('yes, we can use the semicolon in python but when we need it , not in every single statment')
+print('Python is an interpreted language')
+print('What is the difference between high-level and low-level programming language in Python?')
+print('High-level languages require the use of a compiler or an interpreter for their translation into the machine code. Low-level language requires an assembler for directly translating the instructions of the machine language. These languages have a very low memory efficiency.')
