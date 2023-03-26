2e6206a563dc31ee09e024ce5353ddb22e23adb3
ZAK91 <120594345+ZAK91@users.noreply.github.com>
2023-03-26 18:38:17 +0200

    Q3

--- Python new/Operators.py
+++ Python new/Operators.py
@@ -1,3 +1,4 @@
 print('the = is Assignment Operator and the == is comparison operator')
 print('the != means, check if the value of two operands is not equal , if not then it is True')
 print('Operator precedence determines how operators are parsed concerning each other. Operators with higher precedence become the operands of operators with lower precedence.')
+x=10+5
