fd8331c3dd0c2de864a207ee18898337e5647a04
ZAK91 <120594345+ZAK91@users.noreply.github.com>
2023-03-28 19:24:37 +0200

    Q4

--- Python new/Condtion Tasks.py
+++ Python new/Condtion Tasks.py
@@ -4,3 +4,4 @@ res = x > y
 print (res)
 print('x is smaller than 15')
 print('Answer Q3 is : The all() function returns True if all items in an iterable are true, otherwise it returns False')
+x = [2,4,6,8,10]
