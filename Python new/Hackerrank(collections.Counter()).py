3a610f049622437ed3dbbde812d2ff461ae9ecbf
ZAK91 <120594345+ZAK91@users.noreply.github.com>
2023-04-04 23:31:57 +0200

    Hackerrank Tasks

--- Python new/Hackerrank(collections.Counter()).py
+++ Python new/Hackerrank(collections.Counter()).py
@@ -4,3 +4,4 @@ shoe_size_list = Counter(map(int, input('shoes size list').split()))
 no_of_customers = int(input('no_of_customers'))
 earned = 0
 for i in range(no_of_customers):
+    size, price = map(int, input('size and price').split())
