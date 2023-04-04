e53421474285ed4e5f0623084a88941820c75b80
ZAK91 <120594345+ZAK91@users.noreply.github.com>
2023-04-04 23:29:58 +0200

    Hackerrank Tasks

--- Python new/Hackerrank(collections.Counter()).py
+++ Python new/Hackerrank(collections.Counter()).py
@@ -1,3 +1,4 @@
 from collections import Counter
 no_of_shoe = int(input('shoes num'))
 shoe_size_list = Counter(map(int, input('shoes size list').split()))
+no_of_customers = int(input('no_of_customers'))
