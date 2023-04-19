0d839c1fef2ef81f2f9d04487a04ad74a55ae741
ZAK91 <120594345+ZAK91@users.noreply.github.com>
2023-04-19 23:52:50 +0200

    list

--- Python new/Hackerrank(List).py
+++ Python new/Hackerrank(List).py
@@ -3,4 +3,9 @@ y = 1
 z = 1
 n = 2
 
-liste = [ [i,j,k] for i in range (x+1) for j in range (y+1) 
+liste = [ [i,j,k] for i in range (x+1) for j in range (y+1) for k in range (z+1) if i+j+k !=n ]
+
+
+    
+
+    
