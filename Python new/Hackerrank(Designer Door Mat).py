2a9c8eed3c60c449780ffe8a569d9977af920f88
ZAK91 <120594345+ZAK91@users.noreply.github.com>
2023-04-28 16:04:15 +0200

    Hacker Rank Designer Door Mat

---
+++ Python new/Hackerrank(Designer Door Mat).py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+''' x='-'
+y='.|.'
+m=int(input())
+n=int(input())
+for i in range(1, n, 2):
+    print((y*i).center(m, x))
+print("WELCOME".center(m, x))
+for i in range(n, -1, -2):
+    print((y*i).center(m,x))
+'''
+N, M = map(int, input().split())
